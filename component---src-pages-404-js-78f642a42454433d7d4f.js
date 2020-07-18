610d421a2713c2cb8a7452b1a82c4faeec8331ac
LimaGuilherme <guilherme.lb@outlook.com>
2020-07-17 23:01:55 -0300

    First Deploy

---
+++ component---src-pages-404-js-78f642a42454433d7d4f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{136:function(e,t,a){"use strict";a.r(t);var n=a(0),c=a.n(n),l=a(144),o=a(143);t.default=function(){return c.a.createElement(l.a,null,c.a.createElement(o.a,{pageTitle:"Page Not Found"}),c.a.createElement("section",{className:"section"},c.a.createElement("h1",{className:"title has-text-centered"},"Page Not Found")),c.a.createElement("hr",null),c.a.createElement("p",{className:"has-text-centered"},"What you trying to do?"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-78f642a42454433d7d4f.js.map

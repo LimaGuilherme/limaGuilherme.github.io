8d843db88b19a820465de511552a702142dcc04d
LimaGuilherme <guilherme.lb@outlook.com>
2020-07-17 23:41:19 -0300

    Deploy !

---
+++ component---src-pages-404-js-2c2325010c6d962430fe.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{137:function(e,t,a){"use strict";a.r(t);var n=a(0),c=a.n(n),l=a(145),o=a(144);t.default=function(){return c.a.createElement(l.a,null,c.a.createElement(o.a,{pageTitle:"Page Not Found"}),c.a.createElement("section",{className:"section"},c.a.createElement("h1",{className:"title has-text-centered"},"Page Not Found")),c.a.createElement("hr",null),c.a.createElement("p",{className:"has-text-centered"},"What you trying to do?"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2c2325010c6d962430fe.js.map

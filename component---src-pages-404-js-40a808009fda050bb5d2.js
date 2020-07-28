6eee48f453bcb2ed2ed8217b87df12ccea35f861
LimaGuilherme <guilherme.lb@outlook.com>
2020-07-28 16:20:38 -0300

    New version

---
+++ component---src-pages-404-js-40a808009fda050bb5d2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{136:function(e,t,a){"use strict";a.r(t);var n=a(0),c=a.n(n),l=a(145),o=a(144);t.default=function(){return c.a.createElement(l.a,null,c.a.createElement(o.a,{pageTitle:"Page Not Found"}),c.a.createElement("section",{className:"section"},c.a.createElement("h1",{className:"title has-text-centered"},"Page Not Found")),c.a.createElement("hr",null),c.a.createElement("p",{className:"has-text-centered"},"What you trying to do?"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-40a808009fda050bb5d2.js.map
